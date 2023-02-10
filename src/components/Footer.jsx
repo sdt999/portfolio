8c37145f1c3fb719f478bbca82a09dc9bdb16920
Shobhan Das Thakur <shobhandasthakur@gmail.com>
2023-02-10 23:37:02 +0530

    add app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  AiOutlineArrowUp,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+
+        <h2>Shobhan Dasthakur</h2>
+        <p>Motivation is temporary, but discipline last forever.</p>
+      </div>
+
+      <aside>
+        <h2>Social Media</h2>
+
+        
+      </aside>
+      <a href="#home">
+        <AiOutlineArrowUp />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
